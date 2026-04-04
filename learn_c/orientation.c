1d4a62908b0edf9684266166244b0a24cdf5188a
MukitHossen7 <hossenmukit7@gmail.com>
2026-04-04 14:55:45 +0600

    Add initial C and Python programs for user input and greeting

---
+++ learn_c/orientation.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    printf("Hello Mukit");
+    return 0;
+   
+}
---
+++ python/day1.py
@@ -0,0 +1,17 @@
+# print("Hello, World!")
+
+# number = 42
+# number1 = 30
+
+# name = "Mukit Hossen"
+
+# someList = ["Python", "Java", "C++", 5000, 3.14, True]
+
+# print(name)
+# print(number + number1)
+# print(someList)
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print("Hello, " + name + "! You are " + age + " years old.")
