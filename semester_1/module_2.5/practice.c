38616d421eb8e4dbcde15bd9a3ad21d47cddd11e
MukitHossen7 <hossenmukit7@gmail.com>
2026-04-07 00:06:45 +0600

    Add input/output files and initial C program for float handling

---
+++ semester_1/module_2.5/practice.c
@@ -0,0 +1,40 @@
+// problem 1
+
+// #include <stdio.h>
+// int main (){
+//     int n;
+//     scanf("%d", &n);
+//     if(n == 0){
+//         printf("Zero");
+//     }else{
+//         printf("Non Zero");
+//     }
+// }
+
+// problem 2
+// # include <stdio.h>
+// int main (){
+//     int n;
+//     scanf("%d", &n);
+//     int add = n + 5;
+//     printf("%d", add);
+// }
+
+// problem 3
+// # include <stdio.h>
+// int main (){
+//     int a, b;
+//     scanf("%d %d", &a, &b);
+//     if( a % b == 0 || b % a == 0){
+//         printf("Yes");
+//     }else{
+//         printf("No");
+//     }
+// }
+
+# include <stdio.h>
+int main (){
+    float x;
+    scanf("%f", &x);
+    printf("%.3f", x);
+}
